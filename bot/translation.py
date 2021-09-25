11176454435dc777fb325eccdd4f74a33a4a3df4
Stranger N <91267135+adventurboy@users.noreply.github.com>
2021-09-25 17:09:45 +0400

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -45,4 +45,4 @@ class Translation(object):
 <b>➥ 👨‍💻Editor</b> : <b>@no-UFAL</b>
 <b>➥ 🗣️Language</b> : <b>Python3</b>
 <b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MRK-YT/Pro-Auto-Filter-Bot">💥Click Me</a></b>
+<b>➥ 📖Source Code</b> : <b><a href="https://github.com/adventurboy/autofilterbotv2">💥Click Me</a></b>
