fe89b64406058eff23deaa2b4e650e33a5f48681
Stranger N <91267135+adventurboy@users.noreply.github.com>
2021-09-25 18:44:01 +0400

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = "@keralamoviesindian"
+                caption ="@keralamoviesindian",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
