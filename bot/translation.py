a5ab3066864f610ce15e2cf23a0ab94e0186f260
Stranger N <91267135+adventurboy@users.noreply.github.com>
2021-09-25 19:04:27 +0400

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,7 +7,7 @@ class Translation(object):
     START_TEXT = """<b>Hey {} How are You!!</b>
 <i>
 നീ ഏതാ മോനൂസെ എന്നെ
- 𝗔𝗗𝗠𝗢𝗩𝗘𝗜𝗦 𝗛𝗨𝗡𝗧ഗ്രൂപ്പിൽ മാത്രമേ
+POPCORN ഗ്രൂപ്പിൽ മാത്രമേ
 ഉപയോഗിക്കാൻ പറ്റൂ...
 
 @no_ufal ആണ് എന്റെ
@@ -19,18 +19,18 @@ class Translation(object):
     
     HELP_TEXT = """
 <i>
- 𝙄 𝘾𝙖𝙣'𝙩 𝙝𝙚𝙡𝙥 𝙮𝙤𝙪
 
+നോകണ്ട ഉണ്ണി
 
 """
     
     ABOUT_TEXT = """<b>➥ My Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ My Creator</b> : <b><i><a href="https://t.me/Lucifer_Devil_AD">Rafeeq_Kunnimon</a></i></b>
+<b>➥ My Creator</b> : <b><i><a href="https://t.me/no_ufal">Noufal</a></i></b>
 
 <b>➥ My Language</b> : <code>Python3</code>
 
 <b>➥ My Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://telegra.ph/file/65b78cec444fb1fd0dadc.jpg">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://telegra.ph/file/63da9eb63dce040aadf9a.jpg">Click Me</a></i>
 """
